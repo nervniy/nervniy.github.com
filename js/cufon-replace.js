b81fce46e272b273b717e1d3c2547a66b7776dac
nervniy <nervniy.vtsk@mail.ru>
2012-08-03 21:58:53 +0300

    ee
    ee

--- js/cufon-replace.js
+++ js/cufon-replace.js
@@ -1,4 +1,3 @@
-Cufon.replace('h1', { fontFamily: 'Humanst521 Lt BT', color: '-linear-gradient(#ffffff, #cccccc)' });;
-Cufon.replace('header nav a', { fontFamily: 'Humanst521 Lt BT', textShadow: '1px 1px rgba(0, 0, 0, 0.2)' });;
-Cufon.replace('h2', { fontFamily: 'Humanst521 Lt BT'});;
-Cufon.replace('.news li figure', { fontFamily: 'Humanst521 BT'});;
+Cufon.replace('#menu a, h5, .footer_logo', { fontFamily: 'EB Garamond', hover:true });
+Cufon.replace('h2', { fontFamily: 'CabinSketch', hover:true });
+Cufon.replace('h4', { fontFamily: 'CabinSketch', hover:true, textShadow:'#000 0 0' });
